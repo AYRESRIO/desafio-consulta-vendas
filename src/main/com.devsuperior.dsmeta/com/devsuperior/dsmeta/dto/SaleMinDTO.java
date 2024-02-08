2117b54d059638f6a40987437d29119dbe1c31c0
AYRESRIO <83145818+AYRESRIO@users.noreply.github.com>
2024-02-08 07:00:17 -0300

    Implemented sales report.

--- src/main/com.devsuperior.dsmeta/com/devsuperior/dsmeta/dto/SaleMinDTO.java
+++ src/main/com.devsuperior.dsmeta/com/devsuperior/dsmeta/dto/SaleMinDTO.java
@@ -8,20 +8,22 @@ import com.devsuperior.dsmeta.projections.SaleMinProjection;
 public class SaleMinDTO {
 
 	private Long id;
-	private Double amount;
 	private LocalDate date;
+	private Double amount;
 	private String sellerName;
 	
 	
-	public SaleMinDTO(Long id, Double amount, LocalDate date, String sellerName) {
+	
+
+	
+	public SaleMinDTO(Long id, LocalDate date, Double amount, String sellerName) {
 		
 		this.id = id;
-		this.amount = amount;
 		this.date = date;
+		this.amount = amount;
 		this.sellerName = sellerName;
 	}
 
-	
 	public SaleMinDTO(Sale entity) {
 		id = entity.getId();
 		amount = entity.getAmount();
--- src/main/com.devsuperior.dsmeta/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/com.devsuperior.dsmeta/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -20,4 +24,38 @@ public class SaleService {
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
+	
+	public Page<SaleMinDTO>searchReport(String minDate, String maxDate, String name, Pageable pageable){
+		
+		LocalDate min = this.validateMinDate(minDate);
+		LocalDate max = this.validateMaxDate(maxDate);
+		
+		return repository.searchReport(min, max, name, pageable);	
+	}
+
+	private LocalDate validateMaxDate(String maxDate) {
+		LocalDate now = LocalDate.now(ZoneId.of("UTC"));
+		
+		if(maxDate!=null) {
+		return maxDate.equals("")? now : LocalDate.parse(maxDate);
+	}
+		else { 
+			return now;
+			
+		  }
+			
+		}
+
+	private LocalDate validateMinDate(String minDate) {
+		LocalDate now = LocalDate.now(ZoneId.of("UTC"));
+		
+		if(minDate != null) {
+			return minDate.equals("")? now.minusDays(365): LocalDate.parse(minDate);
+		}
+		else { return now.minusDays(365);
+		
+	}
+	
+ }
+	
 }
